19c61879e9caf876264173685f0a7aef53b471f8
Antogor <antoniogarzoniniesta@gmail.com>
2019-12-03 11:37:02 +0100

    intento 1

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 14:16:05 by agarzon-          #+#    #+#             */
-/*   Updated: 2019/12/02 22:15:43 by agarzon-         ###   ########.fr       */
+/*   Updated: 2019/12/03 11:36:44 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 int		get_next_line(int fd, char **line)
 {
+	char			buff[BUFF_SIZE + 1];
+	static char		*s[4096];
+	char			*tmp;
+	int				nbr;
 	
+	if (fd < 0 || line == 0)
+		return (-1);
+	while ((nbr = read(fd, buff, BUFF_SIZE)) > 0);
+	{
+		
+	}
+	return (nbr);
 }
--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
+#include "get_next_line.h"
 
-int	main()
+int	main(int argc, char *argv[])
 {
+	argc = 2;
+	int a = get_next_line(argc, argv);
+	printf("%d\n", a);
 	return (0);
 }
